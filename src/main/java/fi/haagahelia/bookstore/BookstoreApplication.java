219be925bd83ad63475e1368ffd119089ef4f8cd
Pramod <bappramod@gmail.com>
2025-03-05 20:55:38 +0200

    Implemented database-based authentication with BCrypt

--- src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import fi.haagahelia.bookstore.domain.User;
+import fi.haagahelia.bookstore.domain.UserRepository;
 import fi.haagahelia.bookstore.domain.Book;
 import fi.haagahelia.bookstore.domain.BookRepository;
 import fi.haagahelia.bookstore.domain.Category;
@@ -19,8 +21,17 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(CategoryRepository categoryRepo, BookRepository repository) {
+	public CommandLineRunner demo(CategoryRepository categoryRepo, BookRepository repository,
+			UserRepository userRepository) {
 		return (args) -> {
+
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6",
+					"user@bookstore.com", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C",
+					"admin@bookstore.com", "ADMIN");
+			userRepository.save(user1);
+			userRepository.save(user2);
+
 			Category fiction = new Category("Fiction");
 			Category nonFiction = new Category("Non-Fiction");
 			categoryRepo.save(fiction);
--- src/main/java/fi/haagahelia/bookstore/WebSecurityConfig.java
+++ src/main/java/fi/haagahelia/bookstore/WebSecurityConfig.java
@@ -1,59 +1,56 @@
 package fi.haagahelia.bookstore;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
+    // @Autowired
+    private UserDetailsService userDetailsService; // type of attribute -> interface
+
+    // Constructor injection
+    public WebSecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    private static final AntPathRequestMatcher[] WHITE_LIST_URLS = {
+            new AntPathRequestMatcher("/api/booklist**"),
+            new AntPathRequestMatcher("/h2-console/**") };
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/booklist").authenticated()
-                .requestMatchers("/delete/**").hasRole("ADMIN")
-                .anyRequest().permitAll()
-            )
-            .formLogin(login -> login
-                .loginPage("/login")
-                .defaultSuccessUrl("/booklist", true)
-                .permitAll()
-            )
-            .logout(logout -> logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout")
-                .permitAll()
-            );
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(
+                authorize -> authorize
+                        .requestMatchers(antMatcher("/css/**")).permitAll()
+                        .requestMatchers(WHITE_LIST_URLS).permitAll()
+                        .anyRequest().authenticated())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions
+                        .disable())) // for h2console
+                .formLogin(formlogin -> formlogin.loginPage("/login")
+                        .defaultSuccessUrl("/booklist", true)
+                        .permitAll())
+                .logout(logout -> logout.permitAll())
+                .csrf(csrf -> csrf.disable()); // not for production, just for development
 
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-        UserDetails user = User.withUsername("user")
-            .password(encoder.encode("userpass"))
-            .roles("USER")
-            .build();
-
-        UserDetails admin = User.withUsername("admin")
-            .password(encoder.encode("adminpass"))
-            .roles("ADMIN")
-            .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
 }
